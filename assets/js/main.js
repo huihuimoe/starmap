3de19f0179bcb778c9220588420e24d7461683ed
Jian Wang <wjself@qq.com>
2016-06-18 01:24:50 +0800

    bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 var map;
+function removeAnno(code) { //去除注释
+    var reg = /("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))|(\/\*(\n|.)*?\*\/)/g;
+    return code.replace(reg, function (word) {
+        return /^\/{2,}/.test(word) || /^\/\*/.test(word) ? "" : word;
+    });
+}
 function fetchJson(url, callback) {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
-            console.log(request.responseText);
-            callback.call(this, JSON.parse(request.responseText));
+            callback.call(this, JSON.parse(removeAnno(request.responseText)));
         } else {
             alert("地图加载失败啦，按下F5吧~");
         }
@@ -17,11 +22,11 @@ function fetchJson(url, callback) {
 }
 
 function initMap() {
-    fetchJson("assets/data.json", function(data) {
+    fetchJson("assets/data.json", function (data) {
         console.log(data);
     });
     map = new google.maps.Map(document.getElementById('map-content'), {
-        center: {lat: 22.8531358, lng: 111.249542},
+        center: { lat: 22.8531358, lng: 111.249542 },
         zoom: 8
     });
 }
