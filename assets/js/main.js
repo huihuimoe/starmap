358391d6a2c49602a51aa8403c495fdb9abf6579
Jian Wang <wjself@qq.com>
2016-06-18 12:34:04 +0800

    (●^Д^●)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-var map;
-function fetchJson(url, callback) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
+"use strict";
+
+function mapCallBack(data) {
+    var map = new google.maps.Map(document.getElementById('map-content'), {
+        center: { lat: 22.8531358, lng: 111.249542 },
+        zoom: 8
+    });
+
+    // Debug require
+    window.map = map;
+    window.data = data;
+    console.log(data, map);
+}
+
+function initMap() {
+    var request = new XMLHttpRequest(),
+        dataurl = "assets/data.json",
+        callback = mapCallBack;
+    request.open('GET', dataurl, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             callback.call(this, JSON.parse(
@@ -9,21 +24,11 @@ function fetchJson(url, callback) {
                     return /^\/{2,}/.test(word) ? "" : word; //去除注释
                 })));
         } else {
-            alert("地图加载失败啦，按下F5吧~");
+            alert("500啦，按下F5吧~");
         }
     };
     request.onerror = function () {
-        alert("地图加载失败啦，按下F5吧~");
+        alert("请求超时啦，按下F5吧~");
     };
     request.send();
-}
-
-function initMap() {
-    fetchJson("assets/data.json", function (data) {
-        console.log(data);
-    });
-    map = new google.maps.Map(document.getElementById('map-content'), {
-        center: { lat: 22.8531358, lng: 111.249542 },
-        zoom: 8
-    });
 }
