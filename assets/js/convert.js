a8403a28497529623e007a1491032cb59864eae8
Jian Wang <wjself@qq.com>
2016-06-20 01:20:59 +0800

    add data converter && converted data

---
+++ assets/js/convert.js
@@ -0,0 +1,79 @@
+"use strict";
+var route = new Array(), undetermin = new Array();
+
+function convert() {
+    var result = {
+        "current": data.current,
+        "route": route,
+        "undetermin": undetermin
+    };
+    document.getElementById("result").innerHTML = JSON.stringify(result);
+}
+
+function mapCallBack(data, second) {
+    second = 0 || second;
+    if (second) {
+        if (route[0]) return;
+        console.log("dataConvent route start");
+        dataConvent(data, data.route, route);
+    } else {
+        window.data = data;
+        console.log("dataConvent undetermin start");
+        dataConvent(data, data.undetermin, undetermin);
+    }
+}
+
+function dataConvent(data, before, after, index) {
+    index = index || 0;
+    var username = before[index],
+        profile = Object.getOwnPropertyDescriptor(data.profile, username).value,
+        isStop = index === before.length - 1;
+    profile.username = username;
+    getLocation(data, profile, index, before, after, isStop);
+}
+
+function getLocation(data, profile, index, before, after, isStop) {
+    var service = new google.maps.Geocoder();
+    service.geocode({
+        address: profile.location
+    }, function (result, status) {
+        if (status !== "OK") {
+            alert("查询太快了，稍微等待一下……");
+            console.log(status, profile);
+            getLocation(data, profile, index, before, after, isStop);
+            return;
+        }
+        profile.location = result[0].formatted_address;
+        profile.lat = result[0].geometry.location.lat();
+        profile.lng = result[0].geometry.location.lng();
+        after[index] = profile;
+        console.log(index);
+        if (isStop) {
+            console.log("dataConvent complete! please run convert()");
+            mapCallBack(data, true);
+        } else {
+            dataConvent(data, before, after, ++index);
+        }
+    }.bind(this));
+}
+
+function initMap() {
+    var request = new XMLHttpRequest(),
+        dataurl = "assets/data.json",
+        callback = mapCallBack;
+    request.open('GET', dataurl, true);
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            callback.call(this, JSON.parse(
+                request.responseText.replace(/("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))/g, function (word) {
+                    return /^\/{2,}/.test(word) ? "" : word; //去除注释
+                })));
+        } else {
+            alert("500啦，按下F5吧~");
+        }
+    };
+    request.onerror = function () {
+        alert("请求超时啦，按下F5吧~");
+    };
+    request.send();
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,12 @@
 
 function mapCallBack(data) {
     var current = data.current,
-        map, route = new Array(), undetermin = new Array();
+        route = data.route,
+        undetermin = data.undetermin,
+        map;
     map = new google.maps.Map(document.getElementById('map-content'), {
         center: { lat: 23.1259819, lng: 112.9476504 },
         zoom: 10
-    });
-    data.route.forEach(function(username, index){
-        var profile = Object.getOwnPropertyDescriptor(data.profile, username).value;
-        profile.username = username;
-        route[index] = profile;
-    });
-    data.undetermin.forEach(function(username, index){
-        var profile = Object.getOwnPropertyDescriptor(data.profile, username).value;
-        profile.username = username;
-        undetermin[index] = profile;
     });
 
     /**
@@ -36,26 +28,22 @@ function mapCallBack(data) {
      * 其余部分画虚线或者其他表示方式 (选做)
      * 传递状况做一个list就可以
      */
-
+    route.forEach(function(data, index) {
+        
+    });
 
     // Debug require
-    console.log(route, undetermin);
-    window.map = map;
-    window.data = data;
-    //console.log(data, map);
+    console.log(route);
 }
 
 function initMap() {
     var request = new XMLHttpRequest(),
-        dataurl = "assets/data.json",
+        dataurl = "assets/data_convert.json",
         callback = mapCallBack;
     request.open('GET', dataurl, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
-            callback.call(this, JSON.parse(
-                request.responseText.replace(/("([^\\\"]*(\\.)?)*")|('([^\\\']*(\\.)?)*')|(\/{2,}.*?(\r|\n))/g, function (word) {
-                    return /^\/{2,}/.test(word) ? "" : word; //去除注释
-                })));
+            callback.call(this, JSON.parse(request.responseText));
         } else {
             alert("500啦，按下F5吧~");
         }
